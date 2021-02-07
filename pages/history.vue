1ae9dd74e49b784ee21cc8c991bda5846c51638f
Emanuel Ceriana <emanuel.ceriana@raxar.com.ar>
2021-02-06 23:17:02 -0300

    feature: Add historial photo

--- pages/history.vue
+++ pages/history.vue
@@ -1,15 +1,38 @@
 <template>
-  <v-data-table :items-per-page="20" :headers="headers" :items="items">
-  </v-data-table>
+  <div>
+    <v-data-table :items-per-page="20" :headers="headers" :items="items">
+      <template v-slot:item.actions="{ item }">
+        <v-icon v-cloak @click="openDialog(item.path)">mdi-eye</v-icon>
+      </template>
+    </v-data-table>
+    <v-dialog v-model="dialog" persistent max-width="700" clas="border-alert">
+      <v-card>
+        <v-card-title class="justify-center text-center">
+          <img src="~/assets/img/sauron-ico-red.png" width="50px" />
+        </v-card-title>
+        <v-card-text class="justify-center text-center">
+          <img :src="src" />
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn color="red darken-1" text @click="dialog = false">
+            Cerrar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      dialog: false,
+      src: '',
       headers: [
         { text: 'ID Cámara', value: 'camera_id' },
         { text: 'Precisión', value: 'accuracy' },
         { text: 'Fecha', value: 'date' },
+        { text: 'Ver', value: 'actions' },
       ],
       items: [],
     }
@@ -27,6 +50,12 @@ export default {
 
         this.items = result.data.data
       })
+  },
+  methods: {
+    openDialog(path) {
+      this.dialog = true
+      this.src = 'http://localhost:5000/images/' + path
+    },
   },
 }
 </script>
